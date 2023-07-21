ada6c5b2a59b19111ca95b1a7e96f66ff40494a8
aryark <aryark3112@gmail.com>
2023-07-21 10:34:42 +0530

    first commit

---
+++ LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace _21JulyDoublyLinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public void PrintLList()
+        {
+            Node temp = new Node();
+            temp = this.head;
+            if (temp != null)
+            {
+                Console.WriteLine("The List Contains");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("This L List is empty");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21JulyDoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Node first = new Node            
+            {
+                data = 12,
+                next = null,
+                prev = null,
+            };
+            linkedList.head = first;
+            Node second = new Node
+            {
+                data = 20,
+                next = null,
+            };
+            first.next = second;
+            second.prev = first;
+            Node third = new Node
+            {
+                data = 30,
+                next = null
+            };
+            second.next = third;
+            third.prev = second;
+            linkedList.PrintLList();
+            Console.ReadKey();
+        }
+    }
+}
